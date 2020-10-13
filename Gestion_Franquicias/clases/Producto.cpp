64d7ec87538323cc9985f0029162d6bc3ae6a901
Matias Sanchez Villar <67757088+matias-sanchez-villar@users.noreply.github.com>
2020-10-13 02:54:03 -0300

    Modificaciones de producto

--- Gestion_Franquicias/clases/Producto.cpp
+++ Gestion_Franquicias/clases/Producto.cpp
@@ -9,7 +9,7 @@ Producto::Producto(){}
 bool Producto::Cargar_Producto(){
     cout<<"Ingrese el ID ";
     cin>>ID;
-        while(ID<0){ ///validando id que no sea negativo
+        while(ID<0 && setRetornar_ID(ID)==-1){ ///validando id que no sea negativo
             cout<<endl<<"ID incorrecta, reingrese el ID"<<endl<<endl;
             cout<<">> Ingrese el ID: ";
             cin>>ID;
@@ -57,7 +57,7 @@ bool Producto::Cargar_Producto_Disco(){
 return grabo;
 }
 
-bool Producto::Sacar_Disco(int pos){
+bool Producto::setSacar_Disco(int pos){
     bool grabo;
     FILE *p=fopen("producto.dat","rb");
     if(p==NULL){
@@ -68,10 +68,92 @@ bool Producto::Sacar_Disco(int pos){
 return grabo;
 }
 
-void Producto::mostrar(){
+void Producto::mostrar_Producto(){
     cout<<"ID "<<ID;
     cout<<"Nombre "<<Nombre;
     cout<<"Precio "<<Precio;
     cout<<"Cantidad "<<Cantidad;
     cout<<"Cantidad Minima "<<Cantidad_Minima;
 }
+
+void Producto::Modificar_Cantidad(){
+    cout<<"Ingrese el ID ";
+    cin>>ID;
+        while(ID<0 && setRetornar_ID(ID)==-1){ ///validando id que no sea negativo
+            cout<<endl<<"ID incorrecta, reingrese el ID"<<endl<<endl;
+            cout<<">> Ingrese el ID: ";
+            cin>>ID;
+        }
+    mostrar_Producto();
+    cout<<"Ingrese la cantidad: ";
+    cin>>Cantidad;
+        while(Cantidad<0){ ///validando Cantidad que no sea negativo
+            cout<<endl<<"Cantidad incorrecta, reingrese el Cantidad"<<endl<<endl;
+            cout<<">> Ingrese el Cantidad: ";
+            cin>>Cantidad;
+        }
+    ///guardamos en la posicion obtenida
+    int pos=setRetornar_ID(ID);
+    if(setModificar_Producto(pos)==false){
+        cout<<"Se guardo";
+        return;
+    }
+    cout<<"No se guardo";
+}
+
+void Producto::Modificar_Cantidad_Minima(){
+    cout<<"Ingrese el ID ";
+    cin>>ID;
+        while(ID<0 && setRetornar_ID(ID)==-1){ ///validando id que no sea negativo
+            cout<<endl<<"ID incorrecta, reingrese el ID"<<endl<<endl;
+            cout<<">> Ingrese el ID: ";
+            cin>>ID;
+        }
+    mostrar_Producto();
+    cout<<"Ingrese la cantidad minima: ";
+    cin>>Cantidad_Minima;
+        while(Cantidad_Minima<0){ ///validando minima incorrecta que no sea negativo
+            cout<<endl<<"cantidad minima incorrecta, reingrese el cantidad minima"<<endl<<endl;
+            cout<<">> Ingrese el cantidad minima: ";
+            cin>>Cantidad_Minima;
+        }
+    ///guardamos en la posicion obtenida
+    int pos=setRetornar_ID(ID);
+    if(setModificar_Producto(pos)==false){
+        cout<<"Se guardo";
+        return;
+    }
+    cout<<"No se guardo";
+}
+
+bool Producto::setModificar_Producto(int pos){
+    bool correcto;
+    FILE *p=fopen("archivos/usuarios.dat","rb+");
+        if(p==NULL){
+            fclose(p);
+            return false;
+        }
+    fseek(p, pos*sizeof(Producto), SEEK_SET);
+    correcto= fwrite(this, sizeof(Producto), 1, p);
+    fclose(p);
+return correcto;
+}
+
+int Producto::setRetornar_ID(int _ID){
+    int pos=0;
+    FILE *p=fopen("producto.dat","rb");
+        if(p==NULL){
+            fclose(p);
+            return -1;
+        }
+    while(fread(this, sizeof(Producto),1,p)){
+        if(ID==_ID){
+            fclose(p);
+            return pos;
+        }
+        pos++;
+    }
+    fclose(p);
+return -1;
+}
+
--- Gestion_Franquicias/clases/Producto.h
+++ Gestion_Franquicias/clases/Producto.h
@@ -12,8 +12,12 @@ class Producto{
         Producto();
         bool Cargar_Producto();///cargar los valores del producto
         bool Cargar_Producto_Disco();///cargar el producto en disco
-        bool Sacar_Disco(int=0);///obtener x producto de disco con un contador
-        void mostrar();///mostrar producto 1x1
+        bool setSacar_Disco(int=0);///obtener x producto de disco con un contador
+        void mostrar_Producto();///mostrar producto 1x1
+        void Modificar_Cantidad();
+        void Modificar_Cantidad_Minima();
+        bool setModificar_Producto(int);///recive la posicion a modificar
+        int setRetornar_ID(int);///retorna la posicion el producto o -1 si no existe el ID
 };
 
 #endif // PRODUCTO_H_INCLUDED
