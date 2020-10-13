8010e60aee9479656177256fe546197a1849fc93
Matias Sanchez Villar <67757088+matias-sanchez-villar@users.noreply.github.com>
2020-10-13 11:14:56 -0300

    "Producto armado"
    falta probarlo y realizarle muchas mejoras

---
+++ Gestion_Franquicias/Menu_Producto/Menu_Producto.cpp
@@ -0,0 +1,43 @@
+#include <iostream>
+using namespace std;
+#include "Menu_Producto.h"
+#include "../clases/Producto.h"
+
+void Cargar_Producto(){
+    Producto uno;
+    if(uno.Cargar_Producto()==true){
+        if(uno.Cargar_Producto_Disco()==true){
+            cout<<endl<<"PRODUCTO CARGADO"<<endl;
+        }
+    }
+}
+
+void Mostrar_Todos_Producto(){
+    int pos=0;
+    Producto uno;
+    while(uno.setSacar_Disco(pos)!=false){
+        uno.mostrar_Producto();
+        pos++;
+    }
+    cout<<endl<<"TODOS LOS PRODUCTO FUERON MOSTRADOS"<<endl;
+}
+
+void Modificar_Cantidad(){
+    int pos=0;
+    Producto uno;
+    while(uno.setSacar_Disco(pos)!=false){
+        uno.mostrar_Producto();
+        pos++;
+    }
+    cout<<endl<<"TODOS LOS PRODUCTO FUERON MOSTRADOS"<<endl;
+}
+
+void Modificar_Cantidad_minima(){
+    Producto uno;
+    uno.Modificar_Cantidad();
+}
+
+void Modificar_La_Cantidad_minima(){
+    Producto uno;
+    uno.Modificar_Cantidad_Minima();
+}
---
+++ Gestion_Franquicias/Menu_Producto/Menu_Producto.h
@@ -0,0 +1,9 @@
+#ifndef MENU_PRODUCTO_H_INCLUDED
+#define MENU_PRODUCTO_H_INCLUDED
+
+void Cargar_Producto();
+void Mostrar_Todos_Producto();
+void Modificar_Cantidad();
+void Modificar_La_Cantidad_minima();
+
+#endif // MENU_PRODUCTO_H_INCLUDED
