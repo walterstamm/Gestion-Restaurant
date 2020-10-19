56d87b38c3c429e325e347388854bb2722021444
HGOMEZ70 <64662305+HGOMEZ70@users.noreply.github.com>
2020-10-18 22:41:49 -0300

    Hugo sigo conprobantes

--- Gestion_Franquicias/clases/Comprobantes.cpp
+++ Gestion_Franquicias/clases/Comprobantes.cpp
@@ -2,13 +2,23 @@
 #include<cstdlib>
 #include<cstdio>
 #include<cstring>
-
 using namespace std;
+#include "Comprobantes.h"
 
-int main(){
-    int
-    float
-    bool
+///CONTRUCTOR DE LA CLASE COMPROBANTE
+Encabezado::Encabezado(){
+    cout<<"Razon social:           ";
+    cin.getline(Razon_Social, 20);
+    cout<<"Cuit:                   ";
+    cin.getline(Cuit, 13);
+    cout<<"Dirección: Calle y Nro: ";
+    cin.getline(Direccion, 40);
+    cout<<"Localidad y Provincia:  ";
+    cin.getline(Localidad_Provincia, 30);
+    cout<<"Telefono:               ";
+    cin.getline(Telefono, 14);
+    cout<<"Fecha:                  ";
+    Fecha.Cargar_Fecha();
 
-    return 0;
 }
+
