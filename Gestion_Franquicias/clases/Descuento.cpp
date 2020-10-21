1b0378adab7dac8de925af8325940226655dad69
Matias Sanchez Villar <67757088+matias-sanchez-villar@users.noreply.github.com>
2020-10-21 02:12:25 -0300

    Arrancando clase descuento
    ¿que hace validez y que hace estado?

---
+++ Gestion_Franquicias/clases/Descuento.cpp
@@ -0,0 +1,6 @@
+#include <iostream>
+using namespace std;
+#include "Descuento.h"
+
+Descuento::Descuento(){}
+
---
+++ Gestion_Franquicias/clases/Descuento.h
@@ -0,0 +1,13 @@
+#ifndef DESCUENTO_H_INCLUDED
+#define DESCUENTO_H_INCLUDED
+
+class Descuento{
+    private:
+        bool Valides, Estado;
+        float Monto;
+        int ID;
+    public:
+        Descuento();
+};
+
+#endif // DESCUENTO_H_INCLUDED
