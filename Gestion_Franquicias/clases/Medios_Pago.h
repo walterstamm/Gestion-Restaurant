648c56d1418b55847e1d3ee75dede790241ec425
Matias <matias.sanchez@alumnos.frgp.utn.edu.ar>
2020-11-06 00:46:37 -0300

    Generando medio pago

--- Gestion_Franquicias/clases/Medios_Pago.h
+++ Gestion_Franquicias/clases/Medios_Pago.h
@@ -24,8 +24,10 @@ class Medio_Pago{
         void setMonto(float _monto){Monto=_monto;}
         void setDescuento(float _descuento){Descuento=_descuento;}
         void setEstado(bool _Estado){Estado=_Estado;}
+        float Generar_MPago(float); ///genera descuento del medio pago
 };
 
 bool ValidarID_Mpago(int);///recive el ID
+float Generar_MPago(float);/// pide datos para generar el descuento de medio pago y el metodo "Generar_MPago(float)" lo genera y lo valida
 
 #endif // MEDIOS_PAGO_H_INCLUDED
