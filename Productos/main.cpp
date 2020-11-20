04c7b2f8cf2bab7689407ed4e70def5ec6e8c0f9
Matias <matias.sanchez@alumnos.frgp.utn.edu.ar>
2020-11-20 01:01:56 -0300

    alertas listas

--- Productos/main.cpp
+++ Productos/main.cpp
@@ -5,18 +5,40 @@ using namespace std;
 
 #include "Producto.h"
 
+
 int main(){
     Producto uno;
-
     int pos=0;
-    std::map<int, int> vex;
+    std::map<int, int> vexCant_Max;
+    std::map<int, Producto> vexProducto;
     while(uno.LeerPos(pos)){
-        vex[uno.getID()-1]+=uno.getCantidad();
+        vexCant_Max[uno.getID()-1]+=uno.getCantidad();
+        vexProducto[uno.getID()-1]=uno;
         pos++;
     }
     for(int x=0;x<pos;x++){
-        cout<<vex[x]<<endl;
+        if (vexCant_Max[x]>=vexProducto[x].getCantidad_Minima()){
+            ///los que estan en rojo
+            ///Falta listar
+            return 0;
+        }
+        int porcentaje=(vexProducto[x].getCantidad_Minima()*100)/150;
+        if(porcentaje>=vexProducto[x].getCantidad_Minima() && vexCant_Max[x]<vexProducto[x].getCantidad_Minima()){
+            ///alerta amarilla
+            ///falta listar
+            return 0;
+        }
+        if(porcentaje<vexProducto[x].getCantidad_Minima()){
+            ///alerta verde
+            ///falta listar
+            return 0;
+        }
     }
-    cout<<endl<<vex.size();///tamaño del vector.
+
+
+
+
+
+    cout<<endl<<vexCant_Max.size();///tamaño del vector.
 return 0;
 }
